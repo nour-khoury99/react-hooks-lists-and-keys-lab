c98eab08d0204946594bba9193fbd2a313e15481
nour khoury <khourynour79@gmail.com>
2022-10-14 22:06:06 +0300

    done

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,12 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        <ProjectItem name ="Reciplease" about="A receipe Tracking App" />
+        <ProjectItem name =" Kibbles N Bitz" about = "Tinder for Dogs"/>
+        <ProjectItem name = "Alliens Wars" about ="Etsy for Alliens"/>
+        
+      </div>
     </div>
   );
 }
